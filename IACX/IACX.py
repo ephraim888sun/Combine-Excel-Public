a3d76f3840e4278da61628cb599c0f0246253bec
Ephraim.Sun <ephraim888sun@gmail.com>
2021-07-20 17:12:50 -0500

    Update

---
+++ IACX/IACX.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+filename = r'C:\Users\Ephraim.Sun\Desktop\proj3\IACX_final.xlsx'
+
+df_excel = pd.read_excel(filename, index_col=0)
+# print(len(df_excel.columns))
+
+'''Clean Up col names'''
+for col in df_excel.columns:
+    df_excel = df_excel.rename(columns={col: col.replace('\n', '')})
+count = 0
+max_row = 0
+rootdir = r'C:\Users\Ephraim.Sun\Desktop\proj3\IACX'
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        excel_loc = os.path.join(subdir, file)
+        print(excel_loc)
+        if file.endswith(".xlsx"):
+            df = pd.read_excel(excel_loc, index_col=0)
+        else:
+            df = pd.read_csv(excel_loc, index_col=0)
+        df.drop_duplicates()
+        # print(len(df.columns))
+        max_row = max_row + len(df.index)
+        print(max_row)
+        count = count+1
+        print(count)
+        '''Clean Up col names'''
+        for col in df.columns:
+            df = df.rename(columns={col: col.replace('\n', '')})
+
+            if (col == 'FEE_AMOUNT'):
+                df = df.rename(columns={col: col.replace('_',' ')})
+        print(len(df.columns))
+
+        df_excel = pd.concat([df_excel, df], axis=0)
+        # print(len(df_excel.columns))
+
+print(max_row)
+df_excel.to_excel('IACX_final.xlsx', index=True)
+
